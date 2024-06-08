d367658dfce4042156e3b181c236f8cf8ea6fcba
Brogrammer <103189556+BrogrammerMen@users.noreply.github.com>
2024-06-08 21:51:21 +0300

    Add files via upload

---
+++ ITWebsite/JS/main.js
@@ -0,0 +1,7 @@
+let cookies = document.getElementById("cookies");
+let cookiesBtn = document.getElementById("cookiesBtn");
+
+cookiesBtn.addEventListener("click", () => {
+    cookies.style.opacity = "0";
+    cookies.style.translate = "105%";
+});
